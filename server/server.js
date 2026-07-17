050f0d3edf54728aa2c94e981ea1a2e5f2cfd34e
pardhu <pardhu@gmail.com>
2026-07-17 12:24:42 +0530

    Updated server.js for Render

--- server/server.js
+++ server/server.js
@@ -46,15 +46,17 @@ app.get("/", (req, res) => {
    DATABASE
 ========================== */
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("🚀 Server Running on Port 5000");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server Running on Port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("❌ MongoDB Connection Error:", err);
   });
